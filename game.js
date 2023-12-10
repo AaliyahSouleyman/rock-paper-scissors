38d9b08f4456dc965150981c5a2012d253e6ab77
AaliyahSouleyman <80474843+AaliyahSouleyman@users.noreply.github.com>
2023-12-09 19:08:21 -0500

    Create game function and modify playRound function

--- game.js
+++ game.js
@@ -3,27 +3,47 @@ function getComputerChoice() {
     return choices[Math.floor((Math.random()) * 3)]
 }
 
-const playerSelection = 'Paper'
-const computerSelection = getComputerChoice()
-console.log(playRound(playerSelection, computerSelection))
+
+
 
 function playRound(playerSelection, computerSelection){
     let message;
     if (playerSelection.toLowerCase() == computerSelection) {
          message = `Tie Game! ${playerSelection} equals ${computerSelection}. Let's play again!`
          console.log(message)
+         playerSelection = prompt();
+         computerSelection = getComputerChoice();
+         return playRound(playerSelection, computerSelection)
+               
     } else if (playerSelection.toLowerCase() == 'rock' || playerSelection.toLowerCase() == 'scissors' && computerSelection == 'paper') {
+       
         message = `You Win! ${playerSelection} beats ${computerSelection}`
+        console.log(message)
         return message
     } else {
+        
         message = `You Lose! ${computerSelection} beats ${playerSelection}`
+        console.log(message)
         return message
     }
 }
 
-// function game() {
-//     let computerScore, playerScore = 0;
-//     for (let i = 0; i < 5; i++) {
-        
-//     }
-// }
+function game() {
+    let computerScore = 0 
+    let playerScore = 0
+    let playerSelection, computerSelection, winnerMessage
+    for (let i = 0; i < 5; i++) {
+        playerSelection = prompt();
+        computerSelection = getComputerChoice();
+        winnerMessage = playRound(playerSelection, computerSelection)
+        if (winnerMessage.includes('You Win!')) {
+            ++playerScore
+        } else {
+            ++computerScore
+        }
+    }
+
+    return (playerScore > computerScore) ? `You Win! ${playerScore}` : `Computer Wins ${computerScore}`
+}
+
+console.log(game())
